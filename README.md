2476df037ddfbeba67a9efab3872a9b265f58f93
Stijn <stijn.peeters@uva.nl>
2020-06-18 14:27:25 +0200

    Initial commit ✴️

---
+++ README.md
@@ -0,0 +1,37 @@
+# Scrape instagram profiles in batches
+
+This script uses the excellent [instaloader](https://github.io/instaloader) to scrape data for a given list of usernames
+and exports the results as two CSV files and a graph file:
+
+* One CSV file (`accounts.csv`) containing one row per scraped username, with metadata such as biography, number of
+  followers, number of posts, et cetera
+* One CSV file (`posts.csv`) containing one row per post, with metadata such as post URL, caption, hashtags, likes, et
+  cetera
+* One Gephi-compatible GDF file (`follow.gdf`) containing a graph representing followers and followees for each account.
+  This is a directed graph with accounts as nodes, and directed edges representing follower/followee relationships. If
+  account `internetperson` follows account `webuser`, an edge from `internetperson` to `webuser` is created.
+  
+These files will be created in the folder you run the script from.
+
+## Configuration
+This script requires Python 3. It also requires instaloader to work. You can easily install it with pip from the 
+script's directory:
+
+```pip install -r requirements.txt```
+
+You may need to use `pip3` if `pip` does not work or uses Python 2 (you can check this by running `pip -V`). Using this 
+command will ensure the instaloader version required by the script is installed.
+
+Due to Instagram's limitations, the scrape needs to be done 'from' a logged-in account. To this end, you need to create
+a file `config.py` in the script's folder containing your Instagram username and password. You can use 
+`config.py-example` as a template.
+
+Once you have created this file, run the script this way:
+
+`python scrape-batch.py --batchfile batch.txt`
+
+This will scrape all accounts listed in `batch.txt`, assuming one account per line. 
+
+## License etc
+This script was developed by the [Digital Methods Initiative](https://digitalmethods.net) and is licensed with the MIT 
+License. See LICENSE for more information.
---
+++ scrape-batch.py
@@ -0,0 +1,151 @@
+import instaloader
+import argparse
+import pathlib
+import sys
+import csv
+
+config_path = pathlib.Path(__file__, "..", "config.py").resolve()
+if not config_path.exists():
+	print("No config.py file found. You can create it by copying or renaming")
+	print("config.py-example and editing the values in it.")
+	sys.exit(1)
+
+import config
+
+cli = argparse.ArgumentParser()
+cli.add_argument("--batchfile", "-b", required=True, help="File containing entities to scrape, one per line")
+args = cli.parse_args()
+
+batch_file = pathlib.Path(args.batchfile).resolve()
+if not batch_file.exists():
+	print("Batch file ('%s') not found." % str(batch_file))
+	sys.exit(1)
+
+instagram = instaloader.Instaloader()
+try:
+	instagram.login(config.USERNAME, config.PASSWORD)
+except (instaloader.BadCredentialsException, instaloader.InvalidArgumentException):
+	print("Invalid username or password configured in config.py. Cannot scrape.")
+	sys.exit(1)
+except instaloader.TwoFactorAuthRequiredException:
+	print("This account (%s) requires two-factor authentication. The scraper is not" % (config.USERNAME))
+	print("able to handle 2FA at this time. Please disable two-factor authentication for this")
+	print("account and try again.")
+	sys.exit(1)
+
+usernames = set()
+with batch_file.open() as input:
+	for username in input:
+		if not username.strip():
+			continue
+		usernames.add(username.strip())
+
+
+# initialise result variables and files
+# user and post data is written to a CSV file directly, followers/followees are
+# kept in memory until the end to write one big graph file
+user_file = open("accounts.csv", "w")
+user_writer = csv.DictWriter(user_file, fieldnames=["username", "url",
+													"url_profile_pic", "full_name", "userid", "is_verified",
+													"has_viewable_story", "has_public_story",
+													"biography", "media_count", "igtv_count", "followers", "followees"])
+user_writer.writeheader()
+
+post_file = open("posts.csv", "w")
+post_writer = csv.DictWriter(post_file, fieldnames=["shortcode", "username", "date_utc", "url_thumbnail", "url_media",
+													"is_video", "is_sponsored", "hashtags", "mentions", "caption",
+													"video_view_count", "video_length", "likes",
+													"comments", "likes+comments", "location_name", "location_latlong"])
+post_writer.writeheader()
+
+follower_graph = []
+follow_usernames = set()
+
+# start scraping
+print("Scraping %i usernames, %i posts per user." % (len(usernames), config.POSTS_PER_USERNAME))
+for username in usernames:
+	print("Scraping %s..." % username)
+
+	try:
+		profile = instaloader.Profile.from_username(instagram.context, username)
+	except instaloader.ProfileNotExistsException:
+		print("Profile %s does not exist, skipping." % username)
+		continue
+
+	for follower in profile.get_followers():
+		pair = [follower.username, username]
+		follower_graph.append(pair)
+		follow_usernames.add(follower.username)
+		follow_usernames.add(username)
+
+	for followee in profile.get_followees():
+		pair = [username, followee.username]
+		follower_graph.append(pair)
+		follow_usernames.add(followee.username)
+		follow_usernames.add(username)
+
+	profile_info = {
+		"username": username,
+		"url": "https://instagram.com/%s" % username,
+		"url_profile_pic": profile.profile_pic_url,
+		"full_name": profile.full_name,
+		"userid": profile.userid,
+		"is_verified": "yes" if profile.is_verified else "no",
+		"has_viewable_story": "yes" if profile.has_viewable_story else "no",
+		"has_public_story": "yes" if profile.has_public_story else "no",
+		"biography": profile.biography,
+		"media_count": profile.mediacount,
+		"igtv_count": profile.igtvcount,
+		"followers": profile.followers,
+		"followees": profile.followees,
+	}
+
+	user_writer.writerow(profile_info)
+
+	processed = 0
+	for post in profile.get_posts():
+		if processed > config.POSTS_PER_USERNAME:
+			break
+
+		print("...scraping info for post %s" % post.shortcode)
+
+		post_info = {
+			"shortcode": post.shortcode,
+			"username": username,
+			"date_utc": post.date_utc.strftime("%Y-%m-%d %H:%M"),
+			"url_thumbnail": post.url,
+			"url_media": post.video_url if post.is_video else post.url,
+			"is_video": "yes" if post.is_video else "no",
+			"is_sponsored": post.is_sponsored,
+			"hashtags": ",".join(post.caption_hashtags),
+			"mentions": ",".join(post.caption_mentions),
+			"caption": post.caption,
+			"video_view_count": post.video_view_count if post.is_video else 0,
+			"video_length": post.video_duration if post.is_video else 0,
+			"likes": post.likes,
+			"comments": post.comments,
+			"likes+comments": (post.likes + post.comments),
+			"location_name": post.location.name if post.location else "",
+			"location_latlong": " ".join((str(post.location.lat), str(post.location.lng))) if post.location else ""
+		}
+
+		processed += 1
+
+		post_writer.writerow(post_info)
+
+	print("...scraped %i posts for %s" % (processed - 1, username))
+
+user_file.close()
+post_file.close()
+
+print("Done scraping. Writing follower/followee graph.")
+with open("follow.gdf", "w") as output:
+	output.write("nodedef>name VARCHAR\n")
+	for username in follow_usernames:
+		output.write("%s\n" % username)
+
+	output.write("edgedef>from VARCHAR,to VARCHAR,directed BOOLEAN\n")
+	for pair in follower_graph:
+		output.write("%s,%s,true\n" % (pair[0], pair[1]))
+
+print("Done!")
